b64757b7020a155bf9285c5efd0eebdc1947bea5
Brody Berg <brodyberg@gmail.com>
2016-06-27 09:22:45 -0700

    Chapter 7 work

---
+++ Chapter7.hsproj/Arith2.hs
@@ -0,0 +1,12 @@
+module Arith2 where
+  add :: Int -> Int -> Int
+  add x y = x + y
+  
+  addPF :: Int -> Int -> Int
+  addPF = (+)
+  
+  addOne :: Int -> Int
+  addOne = \x -> x + 1
+  
+  addOnePF :: Int -> Int
+  addOnePF = (+1)
