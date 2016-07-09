bbdb3a7e19db9b9594f7017692b7001c882c0714
Brody Berg <brodyberg@gmail.com>
2016-07-09 11:45:03 -0700

    Project deletions & consolidation

---
+++ HaskellProgramming.hsproj/Chapter08/FFS.hs
@@ -0,0 +1,16 @@
+module Chapter08.FFS where
+  myHead [a] = a
+  myHead (x:_) = x
+  
+--fourFactorial :: Integer
+--fourFactorial = 4 * 3 * 2 * 1
+--
+--brokenFact1 :: Integer -> Integer
+--brokenFact1 n = n * brokenFact1 (n - 1)
+
+--module Factorial where
+--  factorial :: Integer -> Integer
+--  factorial 0 = 1
+--  factorial n = n * factorial (n - 1)
+  
+-- let x = []
