4876082872e0d088eb89e7a453a9dfe960466558
Brody Berg <brodyberg@gmail.com>
2016-07-12 13:35:13 -0700

    Extremely satisfying refactoring

--- HaskellProgramming.hsproj/Chapter09/Filtering.hs
+++ HaskellProgramming.hsproj/Chapter09/Filtering.hs
@@ -13,3 +13,103 @@ countOfMultiplesOf3 nums =
 
 -- Next we’re going to work on removing all articles (’the’, ’a’, and ’an’) from sentences. You want to get to something that works like this:--     Prelude> myFilter "the brown dog was a goof"--     ["brown","dog","was","goof"]
 
+filterOutMumbling :: String -> [String]
+filterOutMumbling str = 
+  filter 
+    (\i -> 
+      (all 
+        (\x -> x /= i) 
+        ["the", "a", "an"])) 
+    $ words str
+
+filterOutMumbling' :: String -> String
+filterOutMumbling' str = 
+  concat $ -- merge
+  map (\x -> x ++ " ") $ -- add space
+    filter -- remove mumbling
+      (\i -> 
+        (all 
+          (\x -> x /= i) 
+          ["the", "a", "an"])) 
+      $ words str -- single string to n words
+
+filterOutMumbling'' :: String -> String
+filterOutMumbling'' str = 
+  concat $ -- merge
+  map (++ " ") $ -- add space
+    filter -- remove mumbling
+      (\i -> 
+        (all 
+          (\x -> x /= i) 
+          ["the", "a", "an"])) 
+      $ words str -- single string to n words
+
+--addSpace :: [String] -> String
+--addSpace x = map x ++ " "
+
+--addSpaceToEach :: [String] -> [String]
+--addSpaceToEach x = map (++ " ")
+
+filterOutMumbling3 :: String -> String
+filterOutMumbling3 str = 
+  concat $ -- merge
+  map (++ " ") $ -- add space
+    filter -- remove mumbling
+      (\i -> 
+        (all 
+          (\x -> x /= i) 
+          ["the", "a", "an"])) 
+      $ words str -- single string to n words
+
+removeMatchesTo :: [String] -> [String] -> [String]
+removeMatchesTo blocklist items = 
+  filter -- remove mumbling
+    (\i -> 
+      (all 
+        (\x -> x /= i) 
+        blocklist))
+    items 
+
+removeMumbling = removeMatchesTo ["the", "a", "an"]
+
+filterOutMumbling4 :: String -> String
+filterOutMumbling4 str = 
+  concat $ -- merge
+  map (++ " ") $ -- add space
+  removeMatchesTo ["the", "a", "an"] (words str)
+
+filterOutMumbling5 :: String -> String
+filterOutMumbling5 str = 
+  concat $
+  map (++ " ") $
+  removeMumbling $ 
+  words str
+
+addSpaces :: [String] -> [String]
+addSpaces strs = 
+  map (++ " ") strs
+
+filterOutMumbling6 :: String -> String
+filterOutMumbling6 str = 
+  concat $
+  addSpaces $
+  removeMumbling $ 
+  words str
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
