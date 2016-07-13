4a2d1d26c6301293fc083e628b9e2401d10740e1
Brody Berg <brodyberg@gmail.com>
2016-07-13 08:40:18 -0700

    Parsing strangeness

--- HaskellProgramming.hsproj/Chapter09/Zipping.hs
+++ HaskellProgramming.hsproj/Chapter09/Zipping.hs
@@ -1,26 +1,15 @@
 module Chapter09.Zipping where
 
---  myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
---  myZipWith _ [] [] = []
---  myZipWith _ [] _  = []
---  myZipWith _ _ []  = []
---  myZipWith f (a:myAs) (b:myBs) = 
---    [f a b] ++ (myZipWith f myAs myBs)
-
--- I put the two fn defns together, ahead of their
--- bodies because if I put myZip together and myZipWith
--- together after myZip, myZip acts like myZipWith
--- is part of the same function(!) and fails to 
--- typeCheck. 
+-- There has to be that commented line between the
+-- two functions, otherwise myZip thinks that myZipWith
+-- is part of it and we fail to parse/typecheck. 
 
   myZip :: [a] -> [b] -> [(a,b)]
-  myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
-
---  myZip :: [a] -> [b] -> [(a,b)]
   myZip [] [] = []
   myZip [] _  = []
   myZip _  [] = []
-  myZip (a:myAs) (b:myBs) = [(a, b)] ++ (myZip myAs myBs)--  myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+  myZip (a:myAs) (b:myBs) = [(a, b)] ++ (myZip myAs myBs)  --  
+  myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
   myZipWith _ [] [] = []
   myZipWith _ [] _  = []
   myZipWith _ _ []  = []
