e29a0f08a2bc844c4dfc9120d867f9f4154913d0
Brody Berg <brodyberg@gmail.com>
2016-07-13 21:35:32 -0700

    Recursive capitalize

--- HaskellProgramming.hsproj/Chapter09/Capitalize.hs
+++ HaskellProgramming.hsproj/Chapter09/Capitalize.hs
@@ -4,4 +4,9 @@ module Chapter09.Capitalize where
   capitalize :: String -> String
   capitalize str =
     [toUpper (head str)] ++ (tail str)
+  
+  capitalize' :: String -> String
+  capitalize' [] = []
+  capitalize' str =
+    [toUpper (head str)] ++ capitalize' (tail str)
     
