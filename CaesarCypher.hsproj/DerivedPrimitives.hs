ff85ff5e3cca62f3d364259f437dc9e1b95377b2
Brody Berg <brodyberg@gmail.com>
2016-10-06 13:17:58 -0700

    Chapter 10 Examples

---
+++ CaesarCypher.hsproj/DerivedPrimitives.hs
@@ -0,0 +1,25 @@
+module DerivedPrimitives where
+   
+getLine' :: IO String
+getLine' = do x <- getChar
+              if x == '\n' then 
+                 return []
+              else
+                 do xs <- getLine'
+                    return (x:xs)
+                    
+putStr' :: String -> IO ()
+putStr' []     = return ()
+putStr' (x:xs) = do putChar x
+                    putStr' xs
+
+putStrLn' :: String -> IO ()
+putStrLn' xs = do putStr' xs
+                  putChar '\n'
+                  
+strlen :: IO ()
+strlen = do putStr' "Enter a string: " 
+            xs <- getLine' 
+            putStr' "The string has "
+            putStr' (show (length xs))
+            putStrLn' " characters"
