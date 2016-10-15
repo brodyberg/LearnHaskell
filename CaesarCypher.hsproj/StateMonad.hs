d86474dd1fcbc3a74a15f20fb3bdd27cb99a3d78
Brody Berg <brodyberg@gmail.com>
2016-10-15 15:33:55 -0700

    Cleanup

--- CaesarCypher.hsproj/StateMonad.hs
+++ CaesarCypher.hsproj/StateMonad.hs
@@ -3,63 +3,13 @@ module StateMonad where
 -- import Prelude hiding ((+))
   
 type State = Int
--- the definition below is just identity
--- type ST = State -> State
--- this is identity + a little something
---type ST a = State -> (a,State)
-
-
---foo :: ST
---foo s = s
-
---foo :: ST Int
---foo s = (1,2)
-
 newtype ST a = S (State -> (a,State))
 
-foo'''' :: ST Char -> Char
-foo'''' (S x) = 
+peek :: ST Char -> Char
+peek (S x) = 
   answer
     where (answer,_) = x 3
---  let (answer,_) = x 3
---    answer
---foo'''' (S x) = let (answer,_) = x 3
---foo'''' (S x) = let (answer,_) = x 3 
 
-----newtype Bar = Foo ()
---newtype Bar a = Foo a
---
---foo :: Bar Int -> Bar Int
---foo b = b
---
---foo'' :: Bar Int -> Int
---foo'' (Foo x) = x
---
-newtype Loo a = F (Int -> a)
-
-foo''' :: Loo Char -> Char
--- foo''' (F x) = (\3 -> x)
-foo''' (F x) = x 3
-
---newtype Fab a = Fib (a)
---
---foo' :: Fab Int -> Int
---foo' (Fib x) = x
-
-
-
---foo :: ST Int
--- foo :: ST a
---foo :: ST Int
----- foo s = (3,2)
----- foo (S st) = (3,2)
---foo (S st) = (\st -> (3,2))
-
---
---foo :: ST
---foo s = 
-
---
 --app :: ST a -> State -> (a,State)
 --app (S st) x = st x
 --
