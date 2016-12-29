f0aff9a59d83e8918c19016dcfb3a0974effb67a
Brody Berg <brodyberg@gmail.com>
2016-12-28 22:33:46 -0500

    Project Rosalind count DNA work
    fun

---
+++ Web/sqlitetest/Rosalind/CountDna.hs
@@ -0,0 +1,42 @@
+module CountDna () where
+
+data DNA = A | G | T | C 
+           deriving (Show, Ord, Eq)
+
+letterToDNA :: Char -> Maybe DNA
+letterToDNA 'a' = Just A
+letterToDNA 'g' = Just G
+letterToDNA 't' = Just T
+letterToDNA 'c' = Just C
+letterToDNA _   = Nothing
+
+-- lettersToDNA :: String -> [DNA]
+-- lettersToDNA (x:xs) = case letterToDNA x of
+--                         Nothing -> lettersToDNA xs
+--                         _       -> letterToDNA x : lettersToDNA xs
+
+lettersToDNA :: String -> [DNA]
+lettersToDNA (x:xs) = case letterToDNA x of
+                        Nothing -> lettersToDNA xs
+                        Just A  -> A : lettersToDNA xs
+                        Just G  -> G : lettersToDNA xs
+                        Just T  -> T : lettersToDNA xs  
+                        Just C  -> C : lettersToDNA xs
+
+-- let letters = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+
+-- data DNACount = DNACount { a :: Int, g :: Int, t :: Int, c :: Int } deriving (Show)
+
+-- let defaultDNACount = DNACount 0 0 0 0
+
+-- letters 
+-- & map (\l -> toLower l)
+-- & foldr (\l acc -> case l of 'a' -> acc { a + 1 })
+
+-- x { a = (a x) + 1 }
+
+-- defaultDNACount { (a defaultDNACount) + 1 }
+-- acc { (g acc) + 1 }
+
+-- let lowerCase = map (\l -> toLower l) letters
+
