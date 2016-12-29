b9d8ade2e061ef91eefd5eee6e2567134f703b93
Brody Berg <brodyberg@gmail.com>
2016-12-29 17:30:57 -0500

    Rosalind 2 done

--- Web/sqlitetest/Rosalind/2_DNAtoRNA/DnaToRna.hs
+++ Web/sqlitetest/Rosalind/2_DNAtoRNA/DnaToRna.hs
@@ -8,11 +8,3 @@ swapT l   = l
 
 dnaToRna :: String -> String
 dnaToRna ls = map swapT ls 
-
--- dnaToRna ls =
---     map
---     (\l -> 
---         case l of 
---         'T' -> 'U'
---         _   -> letter)
---     ls
