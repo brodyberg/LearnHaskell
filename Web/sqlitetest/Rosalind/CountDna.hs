23a91ba5fa517ae62d88fd2b6f2ed9a36abbb864
Brody Berg <brodyberg@gmail.com>
2016-12-29 13:06:24 -0500

    My first ever Rosalind problem solved in Haskell!!!

--- Web/sqlitetest/Rosalind/CountDna.hs
+++ Web/sqlitetest/Rosalind/CountDna.hs
@@ -5,7 +5,7 @@ module CountDna
 data DNA = A | G | T | C 
            deriving (Show, Ord, Eq)
 
-data DNACount = DNACount { a :: Int, g :: Int, t :: Int, c :: Int } 
+data DNACount = DNACount { a :: Int, c :: Int, g :: Int, t :: Int } 
                 deriving (Show)
 
 lettersToCounts :: String -> DNACount
@@ -14,9 +14,9 @@ lettersToCounts ls =
     (\letter acc -> 
                 case letter of 
                     'A' -> acc { a = (a acc) + 1 }
+                    'C' -> acc { c = (c acc) + 1 }
                     'G' -> acc { g = (g acc) + 1 }
                     'T' -> acc { t = (t acc) + 1 }
-                    'C' -> acc { c = (c acc) + 1 }
                     _   -> acc)
-    DNACount { a = 0, g = 0, t = 0, c = 0 }
+    DNACount { a = 0, c = 0,  g = 0, t = 0}
     ls
