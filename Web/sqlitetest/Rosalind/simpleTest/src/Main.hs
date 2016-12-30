da38c75bff9b2b8c46ff6353d363298643fccbfa
Brody Berg <brodyberg@gmail.com>
2016-12-29 21:49:15 -0500

    Great progress on 4th Rosalind problem

--- Web/sqlitetest/Rosalind/simpleTest/src/Main.hs
+++ Web/sqlitetest/Rosalind/simpleTest/src/Main.hs
@@ -1,29 +1,11 @@
-module Main (letters, main) where
+module Main where
 
-import Prelude hiding (lookup)
-import qualified Data.Map.Strict as Map
+import RNAtoProtein
 
-letters :: String
-letters = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
+-- letters2 :: String
+-- letters2 = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
 
 main :: IO ()
 main = do
-  print lkup
-  print $ Map.lookup "UUU" lkup
-  where lkup = Map.fromList [("UUU", "F"),    ("CUU", "L"),  ("AUU", "I"),  ("GUU", "V"),
-                        ("UUC", "F"),    ("CUC", "L"),  ("AUC", "I"),  ("GUC", "V"),
-                        ("UUA", "L"),    ("CUA", "L"),  ("AUA", "I"),  ("GUA", "V"),
-                        ("UUG", "L"),    ("CUG", "L"),  ("AUG", "M"),  ("GUG", "V"),
-                        ("UCU", "S"),    ("CCU", "P"),  ("ACU", "T"),  ("GCU", "A"),
-                        ("UCC", "S"),    ("CCC", "P"),  ("ACC", "T"),  ("GCC", "A"),
-                        ("UCA", "S"),    ("CCA", "P"),  ("ACA", "T"),  ("GCA", "A"),
-                        ("UCG", "S"),    ("CCG", "P"),  ("ACG", "T"),  ("GCG", "A"),
-                        ("UAU", "Y"),    ("CAU", "H"),  ("AAU", "N"),  ("GAU", "D"),
-                        ("UAC", "Y"),    ("CAC", "H"),  ("AAC", "N"),  ("GAC", "D"),
-                        ("UAA", "Stop"), ("CAA", "Q"),  ("AAA", "K"),  ("GAA", "E"),
-                        ("UAG", "Stop"), ("CAG", "Q"),  ("AAG", "K"),  ("GAG", "E"),
-                        ("UGU", "C"),    ("CGU", "R"),  ("AGU", "S"),  ("GGU", "G"),
-                        ("UGC", "C"),    ("CGC", "R"),  ("AGC", "S"),  ("GGC", "G"),
-                        ("UGA", "Stop"), ("CGA", "R"),  ("AGA", "R"),  ("GGA", "G"),
-                        ("UGG", "W"),    ("CGG", "R"),  ("AGG", "R"),  ("GGG", "G")]
-  
+  print $ rnaToProtein letters
+  where letters = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
